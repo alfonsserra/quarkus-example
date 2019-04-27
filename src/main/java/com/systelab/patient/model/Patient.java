2c539c0bba7721ec85d03f38970403037b61acdf
alfonsserra <alfonsoserra@gmail.com>
2019-04-27 08:07:15 +0200

    Add transactional and setters

--- src/main/java/com/systelab/patient/model/Patient.java
+++ src/main/java/com/systelab/patient/model/Patient.java
@@ -55,6 +55,15 @@ public class Patient implements Serializable {
         return surname;
     }
 
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname=surname;
+    }
+
+
     public PhoneNumbers getPhones() {
         return PhoneNumbers.of(phones);
     }
--- src/main/java/com/systelab/patient/service/PatientService.java
+++ src/main/java/com/systelab/patient/service/PatientService.java
@@ -6,6 +6,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @ApplicationScoped
@@ -27,11 +28,16 @@ public class PatientService {
         return Patient.of(patient.getName(), patient.getSurname());
     }
 
+    @Transactional
     public Patient add(Patient patient) {
+        System.out.println("Add patient");
+        System.out.println(patient.getName());
+        System.out.println(patient.getSurname());
         em.persist(patient);
         return patient;
     }
 
+    @Transactional
     public Patient delete(Patient patient) {
         em.remove(patient);
         return patient;
